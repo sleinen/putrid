451826d2ab1771c8f4405f8df1f730dfa31d08be
simon.leinen@gmail.com <simon.leinen@gmail.com@461492eb-d586-f880-fcda-ecfc42b509d4>
2010-05-05 22:15:05 +0000

    Initial check-in.

---
+++ trunk/login/telnet.py
@@ -0,0 +1,18 @@
+import sys, pexpect
+from login import LoginMethod
+
+class TelnetLoginMethod(LoginMethod):
+    def login(self, device):
+        cmd = 'telnet'
+        cmd_args = []
+        cmd_args.append(device.name)
+        p = device.process = pexpect.spawn(cmd, cmd_args)
+        if device.debug:
+            p.logfile = sys.stderr
+        p.expect("Username: $")
+        p.sendline(self.username)
+        p.expect("Password: $")
+        p.sendline(self.password)
+        p.expect(">$")
+        hostname = p.before.rpartition("\n")[2]
+        device.fullprompt = hostname + "#$"
