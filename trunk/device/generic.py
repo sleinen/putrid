986d0b1cf48cf4bce90eb1dc94bae5e3a8b5cdae
simon.leinen@gmail.com <simon.leinen@gmail.com@461492eb-d586-f880-fcda-ecfc42b509d4>
2010-05-07 12:28:50 +0000

    Split Cisco-specific code out to device/cisco.py.

--- trunk/device/generic.py
+++ trunk/device/generic.py
@@ -7,8 +7,6 @@ class ManagedDevice:
     def __init__(self, name, conf):
         self.name = name
         self.enablepw = conf.enable_password(name)
-        self.width = 80
-        self.length = 0
         self.child = None
         self.process = None
         self.debug = False
@@ -19,28 +17,13 @@ class ManagedDevice:
             self.login_method = TelnetLoginMethod(self, conf)
     def ssh_login(self):
         self.login_method.login(self)
-    def enable(self):
-        p = self.process
-        p.sendline("enable")
-        p.expect("password: $")
-        p.sendline(self.enablepw)
-        p.expect(self.fullprompt)
+        self.parse_initial_prompt()
     def login(self):
         self.ssh_login()
         self.enable()
-        p = self.process
-        p.sendline("terminal length "+str(self.length))
-        p.expect(self.fullprompt)
-        p.sendline("terminal width "+str(self.width))
-        p.expect(self.fullprompt)
     def command_results(self, command):
         self.process.sendline(command)
         self.process.expect(self.fullprompt)
         return self.process.before
-    def commands(self):
-        return ["show version"]
-        ## return ["show version",
-        ##         "show idprom backplane"]
     def logout(self):
-        self.process.sendline("quit")
-        self.process.expect(pexpect.EOF)
+        pass
