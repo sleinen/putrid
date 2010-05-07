ceab55926d7212c244af97ea96e0d61d04b013a8
simon.leinen@gmail.com <simon.leinen@gmail.com@461492eb-d586-f880-fcda-ecfc42b509d4>
2010-05-07 15:46:19 +0000

    Added FoundryDevice as a variant of CiscoDevice.

---
+++ device/foundry.py
@@ -0,0 +1,24 @@
+import pexpect
+from device.generic import ManagedDevice
+from device.cisco import CiscoDevice
+
+class FoundryDevice(CiscoDevice):
+    """Brocade/Foundry devices are very similar to Cisco IOS devices.
+    We override a few methods where the two differ.
+    """
+    def set_length_width(self, length, width):
+        ## Foundry/Brocade devices don't seem to have "terminal width"
+        p = self.process
+        p.sendline("terminal length "+str(self.length))
+        p.expect(self.fullprompt)
+    def commands(self):
+        return ["show version"]
+    def disable(self):
+        self.process.sendline("quit")
+        self.enabled = False
+    def logout(self):
+        if self.enabled:
+            self.disable()
+        self.process.sendline("exit")
+        self.process.expect(pexpect.EOF)
+        ManagedDevice.logout(self)
